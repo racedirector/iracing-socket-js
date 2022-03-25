1c544dafa97590eeebf78521c68a558906d79360
Justin Makaila <justinmakaila@gmail.com>
2022-03-25 15:21:37 -0400

    Update bob config for emitters

--- bob.config.js
+++ bob.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   ignore: [
-    '@racedirector/iracing-flags-emitter',
-    '@racedirector/iracing-laps-emitter',
-    '@racedirector/iracing-pit-timing-emitter',
-    '@racedirector/iracing-driver-swap-emitter',
-    '@racedirector/iracing-sim-incident-emitter',
     'example-dashboard'
   ]
 }
