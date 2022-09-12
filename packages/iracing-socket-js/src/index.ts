d60eed2ca47883ade575a0772c3ca0e5fda1d044
Justin Makaila <justinmakaila@gmail.com>
2022-09-12 14:35:32 -0400

    clean up/run prettier

--- packages/iracing-socket-js/src/index.ts
+++ packages/iracing-socket-js/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./core";
+export * from "./react";
 export * from "./types";
 export * from "./utilities";
--- packages/iracing-socket-js/src/react/hooks/index.ts
+++ packages/iracing-socket-js/src/react/hooks/index.ts
@@ -1,2 +1,1 @@
-export * from "./useIRacingSocket";
 export * from "./useIRacingSocketConnectionState";
