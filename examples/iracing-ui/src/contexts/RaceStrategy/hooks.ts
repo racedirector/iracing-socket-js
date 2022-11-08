34dafe5312b707cc13f149789bd54443bce9198b
Justin Makaila <justinmakaila@gmail.com>
2022-11-08 10:06:16 -0500

    Remove race strategy context

--- examples/iracing-ui/src/contexts/RaceStrategy/hooks.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import invariant from "ts-invariant";
-import { getRaceStrategyContext } from "./context";
-
-export const useRaceStrategy = () => {
-  const context = useContext(getRaceStrategyContext());
-  invariant(!!context, "Wrap the root component in an <RaceStrategyProvider>");
-  return context;
-};
--- examples/iracing-ui/src/contexts/RaceStrategy/index.tsx
+++
@@ -1,5 +0,0 @@
-export * from "./consumer";
-export * from "./provider";
-export * from "./hooks";
-export { getRaceStrategyContext } from "./context";
-export type { RaceStrategyContextType } from "./context";
