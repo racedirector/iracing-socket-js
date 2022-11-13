c7c99fa35dc9e1375ce849db56650c9560bd008c
Justin Makaila <justinmakaila@gmail.com>
2022-11-13 14:44:13 -0500

    Begin getting rid of the App component that displays containers...

--- examples/iracing-ui/src/containers/App/index.tsx
+++ examples/iracing-ui/src/containers/App/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
 import { Provider } from "react-redux";
 import { ChakraProvider } from "@chakra-ui/react";
 import AppUI from "../../components/App";
@@ -6,17 +6,25 @@ import { IRacingSocketConnectionProvider } from "../../contexts/IRacingSocketCon
 import { IRacing } from "../IRacing";
 import { store } from "../../app/store";
 
-export const App: React.FC<Record<string, never>> = () => {
+const AppProvider: React.FC<PropsWithChildren<Record<string, unknown>>> = ({
+  children,
+}) => {
   return (
     <ChakraProvider>
       <Provider store={store}>
         <IRacingSocketConnectionProvider>
-          <IRacing>
-            <AppUI />
-          </IRacing>
+          <IRacing>{children}</IRacing>
         </IRacingSocketConnectionProvider>
       </Provider>
     </ChakraProvider>
+  );
+};
+
+export const App: React.FC<Record<string, never>> = () => {
+  return (
+    <AppProvider>
+      <AppUI />
+    </AppProvider>
   );
 };
 
