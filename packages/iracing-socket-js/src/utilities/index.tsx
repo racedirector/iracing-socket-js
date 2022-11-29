1fac76be79868b9db3a95cd322c69278e1938d75
Justin Makaila <justinmakaila@gmail.com>
2022-11-29 10:56:37 -0500

    Move utilities into iracing-socket-js package

--- packages/iracing-socket-js/src/utilities/index.tsx
+++ packages/iracing-socket-js/src/utilities/index.tsx
@@ -1,4 +1,4 @@
-import { Driver, Session } from "../types";
+import { Driver, Flags, iRacingData, PaceFlags, Session } from "../types";
 import { canUseSymbol } from "./globals";
 export * from "./position";
 
@@ -119,4 +119,55 @@ export const getStrengthOfDrivers = (drivers: Driver[]) => {
     (MAGIC_NUMBER / Math.log(2)) * Math.log(drivers.length / total);
 
   return strength / 1000;
+};
+
+const bitwiseFlagHasFlag = (bitwise: number, hasFlag: number): boolean =>
+  (bitwise & hasFlag) === hasFlag;
+
+export const flagsHasFlag = (flags: Flags, hasFlags: Flags) =>
+  bitwiseFlagHasFlag(flags, hasFlags);
+
+export const flagsHasFlags = (flags: Flags, ...hasFlags: Flags[]): boolean[] =>
+  hasFlags.map((flag) => flagsHasFlag(flags, flag));
+
+export const flagsHasSomeFlags = (
+  flags: Flags,
+  ...hasFlags: Flags[]
+): boolean =>
+  flagsHasFlags(flags, ...hasFlags).reduce(
+    (someFlags, hasFlag) => someFlags || hasFlag,
+    false,
+  );
+
+export const flagsHasAllFlags = (flags: Flags, ...hasFlags: Flags[]): boolean =>
+  hasFlags.every((flag) => flagsHasFlag(flags, flag));
+
+export const paceFlagsHasPaceFlag = (
+  flags: PaceFlags,
+  hasPaceFlags: PaceFlags,
+) => bitwiseFlagHasFlag(flags, hasPaceFlags);
+
+export const paceFlagsHasPaceFlags = (
+  flags: PaceFlags,
+  ...hasPaceFlags: PaceFlags[]
+) => hasPaceFlags.map((flag) => paceFlagsHasPaceFlag(flags, flag));
+
+export const parseNumberFromString = (sourceValue: string, unit: string) => {
+  const matches =
+    new RegExp(`^.*(?=\\s${unit})`).exec(sourceValue) || Array<string>();
+  if (matches.length > 0) {
+    return parseFloat(matches[0]);
+  }
+
+  return null;
+};
+
+export const parseTrackLength = ({
+  WeekendInfo: { TrackLength: trackLengthString = null } = {},
+}: iRacingData): number | null => {
+  if (typeof trackLengthString === "string") {
+    return parseNumberFromString(trackLengthString, "km") * 1000;
+  }
+
+  return null;
 };
