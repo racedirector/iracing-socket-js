62ae3ac1ee8943a80584f4b90173d98252c171ec
Justin Makaila <justinmakaila@gmail.com>
2022-12-15 12:07:21 -0500

    move env variables

---
+++ packages/iracing-server/src/constants/env.ts
@@ -0,0 +1,3 @@
+export const sourcePath = process.env.NODE_ENV === 'development' ? 'src' : 'build';
+export const host = process.env.HOST || '0.0.0.0';
+export const port = process.env.PORT || '5000';
--- packages/iracing-server/src/index.ts
+++ packages/iracing-server/src/index.ts
@@ -1,26 +1,20 @@
 import * as moduleAlias from 'module-alias';
-const sourcePath = process.env.NODE_ENV === 'development' ? 'src' : 'build';
+import { host, port, sourcePath } from '@server/constants';
 moduleAlias.addAliases({
   '@server': sourcePath,
   '@config': `${sourcePath}/config`,
   '@domain': `${sourcePath}/domain`,
 });
-
 import { createServer } from '@config/express';
 import { AddressInfo } from 'net';
 import http from 'http';
 import { logger } from '@config/logger';
 
-const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || '5000';
-
 async function startServer() {
   const app = createServer();
   const server = http.createServer(app).listen({ host, port }, () => {
     const addressInfo = server.address() as AddressInfo;
-    logger.info(
-      `Server ready at http://${addressInfo.address}:${addressInfo.port}`,
-    );
+    logger.info(`Server ready at http://${addressInfo.address}:${addressInfo.port}`);
   });
 
   const signalTraps: NodeJS.Signals[] = ['SIGTERM', 'SIGINT', 'SIGUSR2'];
